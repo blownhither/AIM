1123dab13c77798011a5eb08c4c37eee41c6235b
ZiYin MA <14307130033@fudan.edu.cn>
2016-10-31 11:13:54 +0800

    init_vectors

--- kern/arch/i386/arch_trap.c
+++ kern/arch/i386/arch_trap.c
@@ -6,6 +6,7 @@
 #include <sys/param.h>
 #include <arch-trap.h>
 #include <arch-mmu.h>
+#include <segment.h>
 
 #define NIDT 256
 
@@ -17,12 +18,19 @@ void trap_init(void) {
 }
 
 void init_vectors() {
+	for(int i=0; i < NIDT; ++i) {
+		SETGATE(idt[i], 0, SEG_KCODE<<3, vectors[i], 0);
+	}
+	SETGATE(idt[T_SYSCALL], 1, SEG_KCODE<<3, vectors[T_SYSCALL], DPL_USER);
 
-
+	// initlock(&ticklock, "time");
 }
 
 void trap(struct trapframe *tf) {
 	if(tf->trapno == T_SYSCALL) {
-		
+
+	}
+	else {
+		panic("Implement me!");
 	}
 }
